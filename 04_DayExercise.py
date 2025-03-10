c7e5db8f3d9f825488e3a478cb2bb72eb667ca89
Santiago491 <up240153@alumnos.upa.edu.mx>
2025-03-09 21:45:16 -0600

    Actividades hasta el dia 4

--- 04_DayExercise.py
+++ 04_DayExercise.py
@@ -1,16 +1,16 @@
 T='Thirty'
 D='Days'
 l='Of'
-P='Python'
+H='Python'
 space=' '
-print(T+space+D+space+l+space+P)
+print(T+space+D+space+l+space+H)
 
 c='Coding'
-F='For'
+J='For'
 A='All'
-print(c+space+F+space+A)
+print(c+space+J+space+A)
 
-company=c+space+F+space+A
+company=c+space+J+space+A
 print(company)
 print(len(company))
 print(company.upper())
@@ -24,4 +24,58 @@ print(pto)
 
 print(company.index(c))
 
-print(company.replace('Coding','Python'))     
+print(company.replace('Coding','Python'))
+
+X=(company.replace('Coding','Python'))
+Y=X.replace('All','Everyone')
+print(Y)
+
+print(company.split())
+
+S="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(S.split(','))
+
+print(company.index(c))
+print('"For" es el carácter en el índice 0')
+print(company.index(A))
+print('"All" es el carácter en el ultimo indice')
+print(company.index(J))
+print('No existe un caracter con indice 10 en  "Coding For All"')
+
+Python='P'
+For='F'
+Everyone='E'
+P_F_E=Python+space+For+space+Everyone
+print(P_F_E)
+
+coding='C'
+For='F'
+All='A'
+C_F_A=coding+space+For+space+All
+print(C_F_A)
+
+print(C_F_A.index('C'))
+print(C_F_A.index('F'))
+print(C_F_A.rfind('l'))
+
+sup='You cannot end a sentence with because because because is a conjunction'
+print(sup.index('because'))
+print(sup.rindex('because'))
+sac=sup[0:30]
+lap=sup[55:90]
+print(sac+space+lap)
+
+print('"Coding For All" no comienza con una subcadena')
+print('"Coding For All" no termina con una subcadena')
+hat='   Coding For All      '
+print(hat[3:19])
+
+YU='30DaysOfPython'
+YA='thirty_days_of_python'
+print(YU.isidentifier())
+print(YA.isidentifier())
+print('La variable thirty_days_of_python se vuelbe verdadera')
+
+DOC=['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+DAC=' '.join(DOC)
+print(DAC)
