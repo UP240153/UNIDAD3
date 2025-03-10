ce37cf5c242141f517b5ae688236400824a84f36
Santiago491 <up240153@alumnos.upa.edu.mx>
2025-03-10 15:32:34 -0600

    Actividades hasta el día 5 ejercicio 9

---
+++ 05_DayExercise.py
@@ -0,0 +1,20 @@
+coma = ", "
+empty_list = list()
+lst = ['Optimus Prime','Bumblebee','Ironhide','Ratchet','Jazz','Megatron','Starscream','Barricade','Brawl','Bonecrusher','Blackout','Scorponok','Frenzy']
+print(len(lst))
+
+First_Transformers= lst[0]
+Second_Transformers= lst[6]
+Trird_Transformers= lst[12]
+print(First_Transformers+coma+Second_Transformers+coma+Trird_Transformers)
+
+mixed_data_types=['Santiago Mendoza','18 años','1.84 metros','Soltero','Encarnación de Díaz, Jalisco, México']
+it_companies=['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+print(mixed_data_types)
+print(it_companies)
+print('La lista tiene',len(it_companies),'compañias')
+
+First_Company= it_companies[0]
+Second_Company= it_companies[3] 
+Third_Company= it_companies[6]
+print(First_Company+coma+Second_Company+coma+Third_Company)
