0cd22cccae81b8075cc6f87a0a0f1c15dd17a34f
Santiago491 <up240153@alumnos.upa.edu.mx>
2025-03-21 19:27:03 -0600

    Actividades hasta el dia 6

--- 06_DayExercise.py
+++ 06_DayExercise.py
@@ -11,3 +11,38 @@ Padres=('Patricia López','Juan Mendoza')
 Familia=HERMANOS+Padres
 tuple(Familia)
 print('Mi Familia está completa es',Familia)
+
+Familia=0
+hermanas=('María Mendoza','Sofía Mendoza','Camila Mendoza','Valentina Mendoza')
+hermanos=('Juan Mendoza','Pedro Mendoza','Carlos Mendoza','Andrés Mendoza','Felipe Mendoza')
+Padres=('Patricia López','Juan Mendoza')
+
+frutas = ("manzana", "plátano", "naranja", "uva", "mango")
+vegetales = ("zanahoria", "brócoli", "espinaca", "tomate", "papa")
+productos_animales = ("leche", "huevo", "queso", "mantequilla", "yogur")
+
+comida_tp = frutas + vegetales + productos_animales
+
+# Imprimiendo el resultado
+print(comida_tp)
+print(tuple(comida_tp))
+
+comida_it= comida_tp
+(tuple(comida_it))
+
+Medio=(comida_tp[7:8])
+print(Medio)
+
+primeros_tres=(comida_tp[0:3])
+print(primeros_tres)
+ultimos_tres=(comida_tp[12:])
+print(ultimos_tres)
+
+comida_tp=0
+print(comida_tp)
+print(comida_it)
+
+paises_nordicos = ('Dinamarca', 'Finlandia', 'Islandia', 'Noruega', 'Suecia')
+tuple(paises_nordicos)
+print("Estonia" in paises_nordicos) 
+print("Islandia" in paises_nordicos)
